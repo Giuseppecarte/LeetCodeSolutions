ca8723fb734d180a14814b64ac5002739040071a
Giuseppe Carte <you@example.com>
2023-07-18 23:36:49 -0600

    Contains Duplicate Solution

---
+++ Easy/Contains_Duplicate.py
@@ -0,0 +1,16 @@
+''' Contains Duplicare
+
+- Runtime: 618 ms 
+
+- Memory Usage: 30.8 MB
+
+        Subission Results 
+
+- Runtime better than 5.7% of the total submissions 
+
+- Memory better than 89.86% of total submissions
+
+'''
+class Solution:
+    def containsDuplicate(self, nums :List[int]) -> bool:
+        return len(nums) != len(set(nums))
